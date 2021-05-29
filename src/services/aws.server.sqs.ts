42ddbffd83541ef25ad639e47bd4308f5defd55f
Efrain Garay <egaray7@gmail.com>
2021-05-29 13:55:57 -0400

    feat(): skeleton server microservices

---
+++ src/services/aws.server.sqs.ts
@@ -0,0 +1,18 @@
+import { CustomTransportStrategy, Server } from '@nestjs/microservices';
+
+export class AwsCloudSqsServer
+    extends Server
+    implements CustomTransportStrategy {
+    /**
+     * This method is triggered when you run "app.listen()".
+     */
+    listen(callback: () => void) {
+        console.log(this.messageHandlers);
+        callback();
+    }
+
+    /**
+     * This method is triggered on application shutdown.
+     */
+    close() {}
+}
